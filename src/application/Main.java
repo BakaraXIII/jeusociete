7efa04e56dd60ec1d8609755439eb5d32cb09c31
rayleigh01 <jordan.long@etu.univ-savoie.fr>
2018-04-11 18:32:20 +0200

    Lecture JSON

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,16 @@
 package application;
 	
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -17,6 +28,23 @@ public class Main extends Application {
 	        stage.setTitle("FXML Welcome");
 	        stage.setScene(scene);
 	        stage.show();
+	        
+	        File file = new File("E:\\Git\\jeusociete\\src\\ressources\\enigme.json"); //lecture du JSON changer le chemin avec celui de chez vou, j'ai mis l'absolu parce que le relatif ne marche pas
+	        
+	        BufferedReader br = new BufferedReader(new FileReader(file));
+	        
+	       String st = "";
+	       while (true) {
+	    	    final String line = br.readLine();
+	    	    if (line == null) break;
+	    	    
+	    	    st += line;
+	    	}
+	        
+	        System.out.println(st);
+	        
+	        JSONArray a = new JSONArray(st);
+	        br.close();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
