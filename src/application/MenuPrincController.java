2ce6826d779f6deb2aec5d476da8dbd676a60250
MRayleigh <jordanlong5991@gmail.com>
2018-04-27 10:53:20 +0200

    Ajout histoire + correction bug

--- src/application/MenuPrincController.java
+++ src/application/MenuPrincController.java
@@ -31,4 +31,67 @@ public class MenuPrincController {
 		}
 	    
 	}
+	
+	public void histoire(ActionEvent e) {
+		System.out.println("Jouer");
+		
+		Node bouton = (Node) e.getSource();
+		Stage theStage = (Stage) bouton.getScene().getWindow(); //garder même fenêtre
+		
+		Parent root;
+		try {
+			root = FXMLLoader.load(getClass().getResource("/Histoire.fxml"));
+			Scene scene = new Scene(root);
+		    
+	        theStage.setTitle("FXML Welcome");
+	        theStage.setScene(scene);
+	        theStage.show();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	    
+	}
+	
+	public void histoire2(ActionEvent e) {
+		System.out.println("Jouer");
+		
+		Node bouton = (Node) e.getSource();
+		Stage theStage = (Stage) bouton.getScene().getWindow(); //garder même fenêtre
+		
+		Parent root;
+		try {
+			root = FXMLLoader.load(getClass().getResource("/Histoire2.fxml"));
+			Scene scene = new Scene(root);
+		    
+	        theStage.setTitle("FXML Welcome");
+	        theStage.setScene(scene);
+	        theStage.show();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	    
+	}
+	
+	public void retour(ActionEvent e) {
+		System.out.println("Jouer");
+		
+		Node bouton = (Node) e.getSource();
+		Stage theStage = (Stage) bouton.getScene().getWindow(); //garder même fenêtre
+		
+		Parent root;
+		try {
+			root = FXMLLoader.load(getClass().getResource("/MenuPrinc.fxml"));
+			Scene scene = new Scene(root);
+		    
+	        theStage.setTitle("FXML Welcome");
+	        theStage.setScene(scene);
+	        theStage.show();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	    
+	}
 }
